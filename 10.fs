ca3afbd251b1943c6b77300a03d9116cc9203727
Michał Kadłubowski <michkad@gazeta.pl>
2020-12-11 04:46:29 +0100

    small improvement

--- 10.fs
+++ 10.fs
@@ -16,16 +16,17 @@ let getPossibilitiesFromPosition (position: int) (joltages: int[]): (int[])=
 
 let solution1 () : int = 
     let input = readLines("data\\10-01.txt") |> Seq.map(Int32.Parse) |> Seq.toArray 
-    let diffs = Array.append [|0|] input |> Array.sort |> Array.pairwise |> Array.map (fun x -> snd x - fst x)
+    let out = (input |> Array.max) + 3
+    let diffs = Array.append [|0; out|] input |> Array.sort |> Array.pairwise |> Array.map (fun x -> snd x - fst x)
     let countOfDiff1 =  diffs |> Array.filter (fun x -> x = 1) |> Array.length   
     let countOfDiff3 = diffs |> Array.filter (fun x -> x = 3) |> Array.length
-    countOfDiff1 * (countOfDiff3 + 1)
+    countOfDiff1 * (countOfDiff3)
 
 let solution2 () = 
     let input = readLines("data\\10-01.txt") |> Seq.map(Int32.Parse) |> Seq.toArray
     let out = (input |> Array.max) + 3
     let inputWithInOut = input |> Array.append [|0; out|] |>  Array.sort
-    let possibilitiesFromPositon = inputWithInOut |> Array.mapi(fun i x-> getPossibilitiesFromPosition i inputWithInOut)
+    let possibilitiesFromPositon = inputWithInOut |> Array.mapi(fun i _-> getPossibilitiesFromPosition i inputWithInOut)
     let possibilitiesArray = Array.foldBack (fun x (acc : int64 list)-> 
                 let indexOfElement = Array.IndexOf(possibilitiesFromPositon,x)
                 let calcRelativeIndex = fun i -> i - indexOfElement - 1
