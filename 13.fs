8e35937a517a0b97c06a183acbdf117c174a832a
Michał Kadłubowski <michkad@gazeta.pl>
2020-12-13 23:25:56 +0100

    day 13

---
+++ 13.fs
@@ -0,0 +1,31 @@
+module day13
+
+open utils
+open System
+            
+let mapToSchedule (lines:string[]) =
+    let time = Int32.Parse lines.[0]
+    let ids = lines.[1].Split ',' |> Seq.filter(fun x -> x <> "x") |> Seq.map(Int32.Parse) |> Seq.toArray
+    (time, ids)
+
+let mapToTimesWithOffsets (lines:string[]) =
+    lines.[1].Split ',' |> Array.mapi(fun i x -> (x,i)) |>
+        Array.filter(fun x -> fst x <> "x") |> Array.map(fun x -> (Int32.Parse (fst x), snd x)) 
+
+let solution1 () =
+    let data = readLines("data\\13-01.txt") |> Seq.toArray |> mapToSchedule
+    let (myTime, ids) = data
+    let closest = ids |> Array.minBy(fun d -> ((myTime/d)+1)* d)
+    closest * (closest-(myTime % closest))
+
+let rec solve2 (data:(int64*int64)[]) (ts:int64) (lcm:int64) : int64 = 
+    let (id,start) = data.[0]
+    let timeSeq = Seq.initInfinite(fun i -> ts + int64 i * lcm) |> Seq.skip(1)
+    let nextMatchingTs = timeSeq |> Seq.find(fun i -> (snd (Math.DivRem((i+start), id)) = 0L))
+    let nextLcm = lcm*id
+    if ((Array.length data) = 1) then nextMatchingTs else  solve2 data.[1..] nextMatchingTs nextLcm
+
+let solution2 () =
+    let data = readLines("data\\13-01.txt") |> Seq.toArray |> mapToTimesWithOffsets |> 
+        Array.map(fun x -> (int64 (fst x), int64 (snd x)))
+    solve2 data 0L 1L
--- utils.fs
+++ utils.fs
@@ -1,6 +1,8 @@
 module utils 
 
 open System.IO
+open Microsoft.FSharp.Core.Operators.Checked
+
 
 let readLines (filePath:string) = seq {
     use sr = new StreamReader (filePath)
@@ -16,3 +18,13 @@ let splitSeq (splitFn) input =
                     !i, x) |>
         Seq.groupBy fst |>
         Seq.map(fun (_,x) -> Seq.map snd x |> Seq.filter (fun s -> not (splitFn s)))
+
+let rec gcd a b = match (a,b) with
+            | (x,y) when x = y -> x
+            | (x,y) when x > y -> gcd (x-y) y
+            | (x,y) -> gcd x (y-x)
+
+let lcm a b = a*b/(gcd a b)  
+
+let lcmOfSeq nums =
+    nums |> Seq.fold(fun acc n -> lcm acc (int64 n)) 1L
